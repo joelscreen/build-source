9893c75b0adc65f97aeb6214f67a05484ff6561e
Joel Mendonca <joelmendonca.2602@gmail.com>
2026-03-19 11:53:04 +0400

    Add debug UI

--- libraries/blocks/blocks.cpp
+++ libraries/blocks/blocks.cpp
@@ -4,6 +4,8 @@
 #include "raymath.h"
 #include <vector>
 #include <algorithm>
+#include <cmath>
+#include <format>
 
 struct BLOCKS {
   Vector3 pos;
@@ -48,14 +50,14 @@ void PlaceBlocks(Camera3D camera, float maxBlockDistance, std::vector<BLOCKS> &b
       float maxDistance = fminf(blockPosBlock.distance, maxBlockDistance);
     if (blockPosBlock.hit && blockPosBlock.distance <= maxDistance) {
       maxDistance = blockPosBlock.distance;
+      Vector3 newPos = Vector3Add(blockPosition.pos, Vector3Scale(blockPosBlock.normal, (float)blockSize));
+      int x = std::round(newPos.x / blockSize) * blockSize;
+      int y = std::round(newPos.y / blockSize) * blockSize;
+      int z = std::round(newPos.z / blockSize) * blockSize;
+      DrawCubeWires(blockPosition.pos, 1.001f, 1.001f, 1.001f, BLACK);
       if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT) && closestBlock) {
-        Vector3 newPos = Vector3Add(blockPosition.pos, Vector3Scale(blockPosBlock.normal, (float)blockSize));
-        for (auto &blocks : blockData) {
-          if (blocks.pos.x == newPos.x && blocks.pos.y == newPos.y && blocks.pos.z == newPos.z) {
-            Vector3Add(newPos, blockPosBlock.normal);
-          }
-        }
-        blockData.push_back(BLOCKS{newPos.x, newPos.y, newPos.z});
+        blockData.push_back(BLOCKS{std::round(newPos.x / blockSize) * blockSize, newPos.y, std::round(newPos.z / blockSize) * blockSize});
+        std::cout << "added\n";
         return;
       }
     }
@@ -79,6 +81,8 @@ void DrawBlocks(Camera3D camera, float maxBlockDistance, std::vector<BLOCKS> blo
   Vector2 screenCenter = {400, 300};
   Ray blockPos = GetMouseRay(screenCenter, camera);
 
+  BLOCKS* closestBlock = nullptr;
+
   for (auto &blockPosition : blockData) {
     BoundingBox bb = BoundingBox{
         Vector3{blockPosition.pos.x - blockSize / 2.0f,
@@ -88,12 +92,27 @@ void DrawBlocks(Camera3D camera, float maxBlockDistance, std::vector<BLOCKS> blo
             blockPosition.pos,
             Vector3{blockSize / 2.0f, blockSize / 2.0f, blockSize / 2.0f})};
     RayCollision blockPosBlock = GetRayCollisionBox(blockPos, bb);
+    float maxDistance = fminf(blockPosBlock.distance, maxBlockDistance);
+    if (blockPosBlock.hit && blockPosBlock.distance <= maxDistance) {
+      Vector3 newPos = Vector3Add(blockPosition.pos, Vector3Scale(blockPosBlock.normal, (float)blockSize));
+      DrawCubeWires(newPos, 1.001f, 1.001f, 1.001f, BLACK);
+      DrawCube(newPos, 1.001f, 1.001f, 1.001f, GREEN);
+      DrawCube(blockPosition.pos, 1.001f, 1.001f, 1.001f, GRAY);
+      DrawCube(blockPosBlock.normal, 0.1f, 0.1f, 0.1f, ORANGE);
 
-    if (blockPosBlock.hit && blockPosBlock.distance == maxBlockDistance) {
-      Vector3 blockNormal = blockPosBlock.normal;
-      Vector3 oldPos = blockPosition.pos;
-      Vector3 newPos = Vector3Add(oldPos, blockNormal);
-      DrawCubeWires(oldPos, 1.0f, 1.0f, 1.0f, BLACK);
+      if (IsKeyPressed(KEY_N)) {
+        std::cout << "Point: " << blockPosBlock.point.x << " " << blockPosBlock.point.y << " " << blockPosBlock.point.z << "\n";
+        Vector3 point = blockPosBlock.point;
+        int x = std::round(point.x / blockSize) * blockSize;
+        int y = std::round(point.y / blockSize) * blockSize;
+        int z = std::round(point.z / blockSize) * blockSize;
+        std::cout << "Calculated Point: " << x << " " << y << " " << z << "\n";
+      }
+      if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT) && closestBlock) {
+        blockData.push_back(BLOCKS{newPos.x, newPos.y, newPos.z});
+        return;
+      }
+      return;
     }
     if (blockPosPlane.hit && blockPosPlane.distance <= maxBlockDistance && !blockPosBlock.hit) {
           Vector3 point = blockPosPlane.point;
@@ -102,9 +121,13 @@ void DrawBlocks(Camera3D camera, float maxBlockDistance, std::vector<BLOCKS> blo
           int z = std::round(point.z / blockSize) * blockSize;
 
           Vector3 pos = Vector3{(float)x, (float)y - 0.49f, (float)z};
-          DrawCubeWires(pos, 1.0f, 1.0f, 1.0f, BLACK);
+          DrawCubeWires(pos, 1.001f, 1.001f, 1.001f, BLACK);
       }
   }
+  for (auto &blockPosition : blockData) {
+    std::cout << "[" << blockPosition.pos.x << ", " << blockPosition.pos.y << ", " << blockPosition.pos.z << "], ";
+  }
+  std::cout << "\n";
 }
 
 void BreakBlocks(Camera3D camera, float maxBlockDistance, std::vector<BLOCKS> &blockData, int blockSize) {
