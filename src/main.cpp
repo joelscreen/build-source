aa49fc7c37d521587b80b72df6aebf62cfabbd48
Joel Mendonca <joelmendonca.2602@gmail.com>
2026-03-28 12:52:21 +0400

    Build alpha-0.0.2-test-2

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 int main() {
   // ----- SETTING UP THE SCREEN -----
-  InitWindow(800, 600, "Build alpha-0.0.1");
+  InitWindow(800, 600, "Build alpha-0.0.2-test-2");
   SetTargetFPS(60);
 
   R3D_Init(800, 600);
